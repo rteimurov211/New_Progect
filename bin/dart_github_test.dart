7740daa387bbffa7e53de6f80343abb14b8c9cd2
rteimurov211 <ruslanteimurov211@gmail.com>
2022-06-12 11:12:21 -0400

    addding

--- bin/dart_github_test.dart
+++ bin/dart_github_test.dart
@@ -1,5 +1,5 @@
 import 'package:dart_github_test/dart_github_test.dart' as dart_github_test;
 
 void main(List<String> arguments) {
-  print('Hello world: Rusimosha');
+  print('Hello world: Rusimosha privet testim vse');
 }
