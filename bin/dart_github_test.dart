bd0e67bcefaa1a481058c9dc317ff4f9873f5a6e
rteimurov211 <ruslanteimurov211@gmail.com>
2022-06-12 11:08:06 -0400

    initial commit

---
+++ bin/dart_github_test.dart
@@ -0,0 +1,5 @@
+import 'package:dart_github_test/dart_github_test.dart' as dart_github_test;
+
+void main(List<String> arguments) {
+  print('Hello world: Rusimosha');
+}
---
+++ test/dart_github_test_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_github_test/dart_github_test.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
